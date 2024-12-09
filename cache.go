29748bbad983bd7bbbb986d30a98cc9a42e06c56
no-regret666 <RangelJara195@gmail.com>
2024-12-09 22:21:47 +0800

    finish 单机并发缓存

---
+++ cache.go
@@ -0,0 +1,33 @@
+package nrCache
+
+import (
+	"nrCache/lru"
+	"sync"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+	if value, ok := c.lru.Get(key); ok {
+		return value.(ByteView), true
+	}
+	return
+}
--- lru/lru_test.go
+++ lru/lru_test.go
@@ -22,7 +22,7 @@ func TestGet(t *testing.T) {
 	}
 }
 
-func TestRemoveoldest(t *testing.T) {
+func TestRemoveOldest(t *testing.T) {
 	k1, k2, k3 := "key1", "key2", "k3"
 	v1, v2, v3 := "value1", "value2", "v3"
 	cap := len(k1 + k2 + v1 + v2)
